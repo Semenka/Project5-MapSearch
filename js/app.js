3b873e7ba1d9f5e5afeefaccbc8cf41028b53cc1
Semenka <andrey.a.semenov@gmail.com>
2016-01-15 23:39:35 +0400

    1st version

---
+++ js/app.js
@@ -0,0 +1,101 @@
+var places_map=[];
+
+function initAutocomplete(){
+	var map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: -33.8688, lng: 151.2195},
+    	zoom: 13,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP
+  	});
+  	// Create the search box and link it to the UI element.
+  	var input = document.getElementById('pac-input');
+  	var searchBox = new google.maps.places.SearchBox(input);
+  	map.controls[google.maps.ControlPosition.LEFT].push(input);
+  	// Bias the SearchBox results towards current map's viewport.
+  	map.addListener('bounds_changed', function() {
+    	searchBox.setBounds(map.getBounds());
+  	});
+  	var markers = [];
+  	// [START region_getplaces]
+  	// Listen for the event fired when the user selects a prediction and retrieve
+  	// more details for that place.
+  	searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+
+
+      places_map.push(place);
+      console.log(place);
+
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+var Place=function(data){
+	this.name=ko.observable(data.name);
+
+}
+
+
+
+var ViewModel=function(){
+	var self=this;
+	this.places=ko.observableArray([]);
+	//Nothing yet in places_map!!!!Add interaction with map
+	places_map.forEach(function(data){
+		
+			self.places.push(new Place(data));
+			//console.log(name);
+		
+	});
+  	
+  // [END region_getplaces]
+	//this.places.push({name:"Moscow"});
+
+
+
+	//this.places=ko.observableArray([]);
+	//this.initAutocomplete();
+	//search_places.forEach(function(place){
+
+	//	self.places.push(new Place(place));
+	//});
+}
+
+
+ko.applyBindings(new ViewModel());
